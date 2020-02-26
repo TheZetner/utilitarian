ab1504e906c75d6b0e153e722e357b8be51fffc8
Adrian Zetner <adrian.zetner@canada.ca>
2020-02-26 17:50:25 -0600

    First functions added with documentation.

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,25 @@
+#' utilitarian: A package for collecting useful R functions
+#'
+#' Eventually useful R scripts have repeated portions that deserve
+#' more than just copying and pasting.
+#'
+#' @section Package Related:
+#' Related somehow to other packages
+#' \itemize{
+#'   \item \code{\link{usePackage}}
+#'   \item \code{\link{libraries}}
+#' }
+#'
+#'
+#' @docType package
+#' @name utilitarian
+NULL
+
+
+
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -9,9 +28,10 @@
 #
 # Some useful keyboard shortcuts for package authoring:
 #
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#   Build and Reload Package:   'Ctrl + Shift + B'
+#   Check Package:              'Ctrl + Shift + E'
+#   Document Package:           'Ctrl + Shift + D'
+#   Test Package:               'Ctrl + Shift + T'
 
 hello <- function() {
   print("Hello, world!")
---
+++ R/package-related.R
@@ -0,0 +1,59 @@
+# Library and package related functions
+
+#' Package installing and loading function
+#'
+#' The usePackage function takes a single command, the name of a package,
+#' and will install it, if not already installed, and then load it.
+#' Taken from \url{https://github.com/sussyfuss/usefulScripts}
+#' @param p A package name as a string in quotes.
+#' @keywords use pack
+#' @export
+#' @examples
+#' usePackage("readr")
+
+usePackage <- function(p) {
+
+  if (!is.element(p, installed.packages()[, 1])) {
+
+    message(paste("Package", p, "not found, installing..."))
+    install.packages(p, dep = TRUE)
+
+  }
+
+  message(paste0("Loading Package ", p, "..."))
+  require(p, character.only = TRUE)
+
+}
+
+#' Call multiple library statements at once
+#'
+#' Attach a number of library packages at once
+#' @author Adrian Zetner
+#' @importFrom dplyr enquos
+#'
+#' @param ... Package names
+#' @param verbose Logical whether to announce via message() what packages are being attached.
+#' @param quoted Logical whether package names supplied bare or quoted as character
+#' @keywords utilitarian library packages
+#' @export
+#' @examples
+#' libraries(rmarkdown, cranlogs, visdat, rentrez)
+#' libraries("rmarkdown", "cranlogs", "visdat", "rentrez", quoted = T)
+#' libraries(c("broom", "forcats"), quoted = T)
+#' libraries(visdat, verbose = TRUE)
+
+libraries <- function(..., verbose = F, quoted = F) {
+  if (quoted) {
+    pkgs <- unlist(list(...))
+  } else {
+    pkgs <- gsub("~", "", as.character(enquos(...)))
+  }
+  sapply(pkgs, library, character.only = T)
+  if (verbose) {
+    message(paste("Attached packages:",
+                  paste(pkgs, collapse = ", ")))
+    }
+}
+
+libraries(rmarkdown, cranlogs)
+sessionInfo()
