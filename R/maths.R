6f835ce85f8a9deb74e207338b0dc42e0b71980e
Adrian Zetner <azetner@cscscience.ca>
2020-05-06 15:23:20 -0500

    Added maths functions: normalize

---
+++ R/maths.R
@@ -0,0 +1,17 @@
+#' Normalize
+#'
+#' Normalizes a vector of values to a range of 0-1
+#' x - min(x)) / (max(x) - min(x)
+#'
+#' @param x Vector of values
+#' @param newlims numeric vector of new minimum and maximum eg. c(2,4)
+#' @return Normalized vector of values to newmax and newmin
+#' @examples
+#' \dontrun{
+#'  normalize(x)
+#'  }
+#' @export
+normalize <- function(x, newlims = c(0,1)){
+  x <- (x - min(x)) / (max(x) - min(x))
+  x * (newlims[2] - newlims[1]) + newlims[1]
+}
