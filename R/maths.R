337bddd55d1337ab104a5ddf4be0c46e1fb9ce52
Adrian Zetner <azetner@cscscience.ca>
2020-05-07 11:06:00 -0500

    added notin

--- R/maths.R
+++ R/maths.R
@@ -15,3 +15,19 @@ normalize <- function(x, newlims = c(0,1)){
   x <- (x - min(x)) / (max(x) - min(x))
   x * (newlims[2] - newlims[1]) + newlims[1]
 }
+
+
+
+#' Not In
+#'
+#' Opposite of \%in\%
+#'
+#' @param x Vector of values
+#' @param y Another vector of values
+#' @return Vector of values that are in X but not in Y
+#' @examples
+#' \dontrun{
+#'  x %!in% y
+#'  }
+#' @export
+`%!in%` <- function(x,y) {!(x %in% y)}
